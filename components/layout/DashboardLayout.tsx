7270108c730b6084086afb9197bd4c83b3bfdd84
mothernatureagency <mn@mothernatureagency.com>
2026-03-29 23:55:38 -0500

    Feat: animated gradient bg in DashboardLayout

--- components/layout/DashboardLayout.tsx
+++ components/layout/DashboardLayout.tsx
@@ -4,13 +4,15 @@ import Header from './Header';
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen" style={{ background: '#f5f7fb' }}>
-      <Sidebar />
-      <div className="flex-1 flex flex-col min-w-0">
-        <Header />
-        <main className="flex-1 px-8 py-7 overflow-auto">
-          {children}
-        </main>
+    <div className="flex min-h-screen dashboard-bg">
+      <div className="relative z-10 flex w-full">
+        <Sidebar />
+        <div className="flex-1 flex flex-col min-w-0">
+          <Header />
+          <main className="flex-1 px-8 py-7 overflow-auto relative z-10">
+            {children}
+          </main>
+        </div>
       </div>
     </div>
   );
